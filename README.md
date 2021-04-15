17f3827690675a8b0775c786406d01b581d0f928
贾腾达 <jiatengda@zjmedia.net>
2021-04-15 17:05:14 +0800

    新增快手去水印功能

--- README.md
+++ README.md
@@ -1,9 +1,15 @@
 # douyin-dewatering
-抖音视频去水印nodejs版
+抖音、快手短视频去水印nodejs版
 
 # 启动方式
-npm install
+npm install 
 npm run dev
 
 # 演示示例
+## 抖音
 ![截图](/images/img.png)
+## 快手
+![截图](/images/ks.png)
+
+# 问题
+目前快手cookie使用的是固定cookie，如无法使用请获取最新cookie进行操作，后续会解决这个问题
--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.all('*', function (req, res, next) {
   res.header('Access-Control-Allow-Methods', '*');
   res.header('Content-Type', 'application/json;charset=utf-8');
   next();
-  });
+});
 
 app.post('/watermark', async function (req, res) {
   const { text } = req.body
@@ -26,10 +26,11 @@ app.post('/watermark', async function (req, res) {
     if (!redirectUrl) {
       return;
     }
-    redirectUrl =  'https://www.iesdouyin.com/web/api/v2/aweme/iteminfo/?item_ids=' + redirectUrl.split('/?region')[0].split('video/')[1]
+    redirectUrl = 'https://www.iesdouyin.com/web/api/v2/aweme/iteminfo/?item_ids=' + redirectUrl.split('/?region')[0].split('video/')[1]
     console.log(19, redirectUrl);
     const resp = await axios.get(redirectUrl)
     let urls = resp.data.item_list[0].video.play_addr.url_list[0]
+    console.log(33, urls);
     if (urls) {
       urls = urls.replace('playwm', 'play')
     }
@@ -38,6 +39,48 @@ app.post('/watermark', async function (req, res) {
       respCode: 200,
       respData: urls
     })
+  }
+})
+
+app.post('/ksVideo', async function (req, res) {
+  const { text } = req.body
+  const reg = /(http|https):\/\/[\w\-_]+(\.[\w\-_]+)+([\w\-\.,@?^=%&:/~\+#]*[\w\-\@?^=%&/~\+#])?/g
+  if (text.match(reg)) {
+    const url = text.match(reg)[0]
+    const data = await axios.get(url)
+    // console.log(50, data);
+    let redirectUrl = data.request.res.responseUrl
+    let hotoId = redirectUrl.substring(redirectUrl.indexOf('hotoId=') + 7, redirectUrl.indexOf('&shareId'))
+    if (hotoId) {
+      const headers = {
+        "Cookie": "did=web_fd7cf10cb70649ef931861163d36e065; didv=1618281519000; kpf=PC_WEB; kpn=KUAISHOU_VISION; clientid=3; ktrace-context=1|MS43NjQ1ODM2OTgyODY2OTgyLjIzMjQ1MjI4LjE2MTg0NzAzODYwMjguMzczNDQ=|MS43NjQ1ODM2OTgyODY2OTgyLjk3MjczNDgxLjE2MTg0NzAzODYwMjguMzczNDU=|0|graphql-server|webservice|false|NA",
+        "Referer": redirectUrl,
+      }
+      
+      const req = {
+        "operationName": "visionVideoDetail",
+        "query": "query visionVideoDetail($photoId: String, $type: String, $page: String, $webPageArea: String) {  visionVideoDetail(photoId: $photoId, type: $type, page: $page, webPageArea: $webPageArea) {    status    type    author {      id      name      following      headerUrl      __typename    }    photo {      id      duration      caption      likeCount      realLikeCount      coverUrl      photoUrl      liked      timestamp      expTag      llsid      viewCount      videoRatio      stereoType      manifest {        mediaType        businessType        version        adaptationSet {          id          duration          representation {            id            defaultSelect            backupUrl            codecs            url            height            width            avgBitrate            maxBitrate            m3u8Slice            qualityType            qualityLabel            frameRate            featureP2sp            hidden            disableAdaptive            __typename          }          __typename        }        __typename      }      __typename    }    tags {      type      name      __typename    }    commentLimit {      canAddComment      __typename    }    llsid    __typename  }}",
+        "variables": {
+          "photoId": hotoId,
+          "page": "detail",
+          "utmCampaign": "app_share",
+          "utmMedium": "app_share",
+          "utmSource": "app_share",
+          "pcursor": ""
+        }
+      }
+      const resp = await axios({
+        method: 'post',
+        headers,
+        url: 'https://video.kuaishou.com/graphql',
+        data: req
+      })
+      const ksUrl = resp.data.data.visionVideoDetail.photo.photoUrl
+      res.json({
+        respCode: 200,
+        respData: ksUrl
+      })
+    }
   }
 })
 
